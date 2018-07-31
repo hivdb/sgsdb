57331e275ab3514d11cbc4a5a0ff39fe8b4d209e
Philip Tzou <philiptz@stanford.edu>
2018-07-31 15:30:17 -0700

    Add references to meta.json

--- scripts/build_db.py
+++ scripts/build_db.py
@@ -10,7 +10,10 @@ import sys
 import csv
 import json
 
+import requests
 from collections import OrderedDict
+
+ESUMMARY_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi'
 
 
 def fasta_reader(filename):
@@ -31,9 +34,34 @@ def fasta_reader(filename):
             yield header, ''.join(seq)
 
 
+def retrieve_references(pubmed_ids):
+    resp = requests.post(
+        ESUMMARY_URL,
+        data={
+            'db': 'pubmed',
+            'id': ','.join(pubmed_ids),
+            'retmode': 'json'
+        }
+    )
+    data = resp.json()
+    references = {}
+    for uid in data['result']['uids']:
+        ref = data['result'][uid]
+        references[uid] = {
+            'MedlineID': uid,
+            'PubYear': ref.get('pubdate', '').split(' ')[0],
+            'firstAuthor': ref['authors'][0]['name'],
+            'Title': ref['title'],
+            'Journal': ref['source']
+        }
+    return references
+
+
 def main():
     facttable, fasta, sierra_report, outputdir = sys.argv[1:]
     with open(facttable) as fp1, open(sierra_report) as fp2:
+        if fp1.read(1) != '\ufeff':
+            fp1.seek(0)
         sequences = list(csv.DictReader(fp1))
         sequence_reports = json.load(fp2)
     sequence_reports = {
@@ -41,8 +69,10 @@ def main():
         for sr in sequence_reports
     }
     result_sequences = []
+    pubmed_ids = set()
     for seq in sequences:
         seq = OrderedDict(seq)
+        pubmed_ids.add(seq['MedlineID'])
         accs = seq['Accession']
         seqreport = sequence_reports[accs]
         subtype = seqreport['subtypeText'].split(' (', 1)[0]
@@ -58,7 +88,7 @@ def main():
         result_sequences.append(seq)
     result_data = {
         'sequences': result_sequences,
-        'references': []
+        'references': retrieve_references(pubmed_ids)
     }
     factjson = os.path.join(outputdir, 'meta.json')
     with open(factjson, 'w') as outfp:
